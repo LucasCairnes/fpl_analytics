79299697f93b7c5eef4d6786e9d680a0e4545488
Lucas <lucas@evidentinsights.com>
2026-02-27 20:38:30 +0000

    Initial test script

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import requests
+import json
+
+def fetch_fpl_data():
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        filename = "fpl_raw_data.json"
+        
+        with open(filename, "w") as file:
+            json.dump(data, file)
+        
+        print(f"Data fetched and saved to {filename}")
+
+    else:
+        print(f"Failed to fetch data. Error code: {response.status_code}")
+
+if __name__ == "__main__":
+    fetch_fpl_data()
