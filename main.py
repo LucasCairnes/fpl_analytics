fef9a3f1274e523862bd37dadd96c13789908152
Lucas <lucas@evidentinsights.com>
2026-02-27 22:24:44 +0000

    Created initial test pipeline to upload raw FPL data to GCP

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-import requests
-import json
-import pandas as pd
+from extract_fpl_data import run_fpl_pipeline
 
-def fetch_fpl_data():
-    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        filename = "fpl_raw_data.json"
-        
-        with open(filename, "w") as file:
-            json.dump(data, file)
-        
-        print(f"Data fetched and saved to {filename}")
+run_fpl_pipeline()
 
-        players_df = pd.DataFrame(data['elements'])
-        print(players_df.head())
-
-    else:
-        print(f"Failed to fetch data. Error code: {response.status_code}")
-
-if __name__ == "__main__":
-    fetch_fpl_data()
