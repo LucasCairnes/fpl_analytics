fc2c7fedf652bf19f436962c8cad94202f0e4045
Lucas <lucas@evidentinsights.com>
2026-02-28 14:49:19 +0000

    generalising bq load function

--- extract/load_to_bq.py
+++ extract/load_to_bq.py
@@ -5,16 +5,13 @@ from datetime import date
 from google.api_core import exceptions
 import pandas_gbq
 
-from gcp_auth import get_fpl_bucket, get_fpl_api_paths, get_credentials
+from gcp_auth import get_fpl_api_paths, get_credentials
 
-player_path = get_fpl_api_paths(["player"])[0]
-bucket = get_fpl_bucket()
+def gcs_to_bq(bucket, gcs_path, table_id):
+    chosen_blob = bucket.blob(gcs_path)
+    chosen_json = chosen_blob.download_as_text()
 
-player_blob = bucket.blob(player_path)
-player_json = player_blob.download_as_text()
+    df = pd.read_json(io.StringIO(chosen_json))
+    project_id = "fpl-analytics-488811"
 
-player_df = pd.read_json(io.StringIO(player_json))
-project_id = "fpl-analytics-488811"
-table_id = f"raw_player_data.players_{date.today().strftime("%Y_%d_%d")}"
-
-pandas_gbq.to_gbq(player_df, table_id, project_id=project_id, credentials=get_credentials(), if_exists="replace")
+    pandas_gbq.to_gbq(df, table_id, project_id=project_id, credentials=get_credentials(), if_exists="replace")
