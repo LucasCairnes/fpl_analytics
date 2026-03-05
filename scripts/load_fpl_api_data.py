5fc33b9294f6fb47146ebba970ac769952966aed
Lucas <lucas@evidentinsights.com>
2026-03-05 14:29:50 +0000

    Completed base of pipeline. Load FPL API data in completely functional state.

--- scripts/load_fpl_api_data.py
+++ scripts/load_fpl_api_data.py
@@ -1,7 +1,10 @@
 from src.extract.fpl_api import run_fpl_pipeline
 from src.load.bq_functions import gcs_to_bq
+from dotenv import load_dotenv
 
 try:
+    load_dotenv()
+
     run_fpl_pipeline()
 
     gcs_path = f"raw-fpl-team/raw-teams-2026-02-28.json"
@@ -12,7 +15,7 @@ try:
     table_id = "fpl-analytics-488811.raw_player_data.full_player_data"
     gcs_to_bq(gcs_path, table_id)
     
-    print("Upload successful!")
+    print("Pipeline executed successfully!")
 
 except Exception as e:
-    print(f"Pipeline encountered unexpected error: {e}")
+    print(f"Load FPL data API encountered unexpected error: {e}")
--- src/load/bq_functions.py
+++ src/load/bq_functions.py
@@ -1,6 +1,4 @@
 from google.cloud import storage
-from datetime import date
-from google.oauth2 import service_account
 import pandas as pd
 import pandas_gbq
 import io
@@ -8,6 +6,7 @@ import io
 from src.load.gcs_functions import get_fpl_bucket
 
 def gcs_to_bq(gcs_path, table_id):
+    print(f"Uploading {gcs_path} to {table_id}...")
     bucket = get_fpl_bucket()
     chosen_blob = bucket.blob(gcs_path)
     chosen_json = chosen_blob.download_as_text()
@@ -15,4 +14,5 @@ def gcs_to_bq(gcs_path, table_id):
     df = pd.read_json(io.StringIO(chosen_json))
     project_id = "fpl-analytics-488811"
 
-    pandas_gbq.to_gbq(df, table_id, project_id=project_id, credentials=get_credentials(), if_exists="replace", location='europe-west2')
+    pandas_gbq.to_gbq(df, table_id, project_id=project_id, if_exists="replace", location='europe-west2')
+    print("Upload successful.")
