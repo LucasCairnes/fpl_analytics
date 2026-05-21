3408c2f3336986bbd48adb1db43678110c52f585
Lucas <lucas@evidentinsights.com>
2026-05-21 18:48:34 +0100

    Changed to delimited json upload

--- code/src/load/gcs_functions.py
+++ code/src/load/gcs_functions.py
@@ -4,4 +4,5 @@ from tenacity import retry, wait_exponential, stop_after_attempt
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(min=2, max=10), reraise=True)
 def load_to_storage(bucket, destination_path, raw_data):
     blob = bucket.blob(destination_path)
-    blob.upload_from_string(json.dumps(raw_data), content_type="application/json")
+    ndjson_data = "\n".join([json.dumps(record) for record in raw_data])
+    blob.upload_from_string(ndjson_data, content_type="application/x-ndjson")
