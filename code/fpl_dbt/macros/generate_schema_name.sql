939716cada3f814e2eeade5942836fe36178fe78
Lucas <lucas@evidentinsights.com>
2026-05-21 13:06:17 +0100

    macro to fix table names

--- code/fpl_dbt/macros/generate_schema_name.sql
+++ code/fpl_dbt/macros/generate_schema_name.sql
@@ -1,14 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
+    
     {%- if custom_schema_name is none -%}
-
+        {# If no schema is defined in dbt_project.yml, use the default target #}
         {{ default_schema }}
-
     {%- else -%}
-
+        {# If a custom schema IS defined, use EXACTLY that name and nothing else #}
         {{ custom_schema_name | trim }}
-
     {%- endif -%}
 
 {%- endmacro %}
