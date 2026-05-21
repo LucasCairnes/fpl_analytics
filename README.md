3bb2cc9e89c309f31440248a1469f2f8e9ccfdbc
Lucas <lucas@evidentinsights.com>
2026-05-21 13:48:12 +0100

    Wrote readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# FPL Analytics
+
+A Fantasy Premier League data analysis project with Python and dbt.
+
+## Structure
+- code/main.py - the main orchestrator for the pipeline
+- code/scripts/ — contains the data extraction pipelines along and script to carry out dbt transforms
+- code/src/ — contains all used ELT functions and all used sql queries
+- code/fpl_dbt/ — contains the dbt models, macros, and sources
+- co
+
+## Requirements
+- Python 3.13+
+- dbt
+- An active google cloud project
+
+## Setup
+1. Create a venv and install the dependencies:
+    conda create -n "fpl_analytics" python=3.13
+    pip install -r requirements.txt
+
+2. Configure dbt connection:
+   - Edit fpl_dbt/profiles.yml or place credentials in ~/.dbt/profiles.yml
+
+3. Configure GCP credentials
+    export GOOGLE_APPLICATION_CREDENTIALS="/path/to/key.json"
+
+4. To run the full pipeline
+    cd code
+    python -m scripts.main
+
--- code/src/queries/player_ids.sql
+++ code/src/queries/player_ids.sql
@@ -1,18 +1,18 @@
--- WITH todays_teams AS (
---     SELECT
---     team_h AS team_id
---     FROM `fpl-analytics-488811.stg_fixture.stg_fixture_data`
---     WHERE date = DATE_SUB(CURRENT_DATE('Europe/London'), INTERVAL 1 DAY )
+WITH todays_teams AS (
+    SELECT
+    team_h AS team_id
+    FROM `fpl-analytics-488811.stg_fixture.stg_fixture_data`
+    WHERE date = DATE_SUB(CURRENT_DATE('Europe/London'), INTERVAL 1 DAY )
 
---     UNION ALL
+    UNION ALL
 
---     SELECT
---     team_a AS team_id
---     FROM `fpl-analytics-488811.stg_fixture.stg_fixture_data`
---     WHERE date = DATE_SUB(CURRENT_DATE('Europe/London'), INTERVAL 1 DAY )
--- )
+    SELECT
+    team_a AS team_id
+    FROM `fpl-analytics-488811.stg_fixture.stg_fixture_data`
+    WHERE date = DATE_SUB(CURRENT_DATE('Europe/London'), INTERVAL 1 DAY )
+)
 
 SELECT
 DISTINCT(player_id)
 FROM `fpl-analytics-488811.curated_player.cur_player_taxonomies`
--- WHERE team_id IN (SELECT team_id FROM todays_teams)
+WHERE team_id IN (SELECT team_id FROM todays_teams)
